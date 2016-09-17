5f6ab862ddf2d6b948931c38322cae6e86d31326
Bosh Liu <jollier.liu@gmail.com>
2016-09-17 12:25:18 -0700

    new file:   eCAVIAR/extract_gwas_variants.R

---
+++ eCAVIAR/extract_gwas_variants.R
@@ -0,0 +1,19 @@
+# extract GWAS hits from the supplmentary excel in the paper Nikpay 2015 Nature Genetics
+library(XLConnect)
+
+# command args: 
+in_file="/srv/persistent/bliu2/HCASMC_eQTL/data/gwas/nikpay_2015_ng.xlsx"
+
+# read gwas loci:
+gwas_fdr_hits=readWorksheet(loadWorkbook(in_file),sheet=7,startRow=2,endRow=204,startCol=1,endCol=3,check.names=F,header=T)%>%select(markername,chr,pos=bp_hg19)
+gwas_genowide_hits=readWorksheet(loadWorkbook(in_file),sheet=6,startRow=3,endRow=59,startCol=2,endCol=4,check.names=F,header=T)%>%select(markername=SNP,chr=CHR,pos=`Base-pair Position`)
+
+
+# merge two lists: 
+merged=rbind(gwas_fdr_hits,gwas_genowide_hits)
+
+# take unique hits: 
+uniq=unique(merged)
+
+# output the merged unique hits:
+write.table(uniq,'../processed_data/eCAVIAR/gwas_loci.cad.all.genomewide_fdr_merged.txt',sep='\t',row.names=F,quote=F)
