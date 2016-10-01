7fa7c0a87dcd6e7bf26162111117666548a0eb4f
Bosh Liu <jollier.liu@gmail.com>
2016-10-01 16:29:24 -0700

    modified:   160603/mds.R 	modified:   egenes_vs_sample_size/plot_num_egenes_subsampled_52.R

--- 160603/mds.R
+++ 160603/mds.R
@@ -6,21 +6,24 @@
 # Rscript hclust.R -rpkm=<rpkm_file> -coldata=<coldata_file> -figure=<out_figure>
 
 # library:
+library(dplyr)
+library(data.table)
 library('R.utils')
 library('MASS')
 library('RColorBrewer')
-
+library(cowplot)
+library(ggrepel)
 
 # command line arguments: 
 args=commandArgs(T,T)
 rpkm_file=args$rpkm
 coldata_file=args$coldata
 tissue_names_file=args$tissue_names
-figure_file=args$figure
+figure_path=args$figure
 # rpkm_file='/srv/persistent/bliu2/HCASMC_eQTL/processed_data/160603/combined.rpkm'
 # coldata_file='/srv/persistent/bliu2/HCASMC_eQTL/processed_data/160603/combined.col'
-# tissue_names_file='/srv/persistent/bliu2/HCASMC_eQTL/scripts/160603/collapsed_tissue_names.txt'
-# figure_file='/srv/persistent/bliu2/HCASMC_eQTL/figures/160603/mds.pdf'
+# tissue_names_file='/srv/persistent/bliu2/HCASMC_eQTL/scripts/160603/collapsed_tissue_names.2.txt'
+# figure_path='/srv/persistent/bliu2/HCASMC_eQTL/figures/160603/'
 
 
 # read input: 
@@ -70,10 +73,19 @@ pal=pal(length(levels(tissue)))
 
 # make scatter plot:
 message('plotting...')
-pdf(figure_file,height=8,width=8)
-par(mar=c(5.1,4.1,4.1,7.1))
-par(xpd=T) # turn of clipping to allow legend outside of plotting area
-plot(mds_res$points,col=pal[color],pch=19,xlab='Coordinate 1',ylab='Coordinate 2')
-legend(0.32,0.3,levels(tissue),col=pal[unique(color)],pch=19,bty='n')
-text(x=mds$x_centroid,y=mds$y_centroid,labels=mds$tissue_abb,cex=0.7)
-dev.off()
+# pdf(figure_file,height=8,width=8)
+# par(mar=c(5.1,4.1,4.1,7.1))
+# par(xpd=T) # turn of clipping to allow legend outside of plotting area
+# plot(mds_res$points,col=pal[color],pch=19,xlab='Coordinate 1',ylab='Coordinate 2')
+# legend(0.32,0.3,levels(tissue),col=pal[unique(color)],pch=19,bty='n')
+# text(x=mds$x_centroid,y=mds$y_centroid,labels=mds$tissue_abb,cex=0.7)
+# dev.off()
+
+set.seed(42)
+mds_centroid=unique(mds[,.(tissue,x_centroid,y_centroid)])
+p1=ggplot(mds,aes(x=x,y=y,color=tissue))+geom_point()+geom_text_repel(data=mds_centroid,aes(x=x_centroid,y=y_centroid,label=tissue),color='black')+theme_bw()
+save_plot(paste0(figure_path,'/mds.pdf'),p1,base_height=6,base_width=8.5)
+p2=ggplot(mds,aes(x=x,y=y,color=tissue))+geom_point()+xlim(-0.1,0.1)+ylim(-0,0.2)+geom_text_repel(data=mds_centroid,aes(x=x_centroid,y=y_centroid,label=tissue),color='black')+theme_bw()+scale_color_discrete(guide=F)
+save_plot(paste0(figure_path,'/mds.cropped.pdf'),p2)
+
+
--- egenes_vs_sample_size/plot_num_egenes_subsampled_52.R
+++ egenes_vs_sample_size/plot_num_egenes_subsampled_52.R
@@ -1,5 +1,7 @@
 # library
+library(data.table)
 library(dtplyr)
+library(dplyr)
 library(cowplot)
 library(stringr)
 
@@ -18,14 +20,27 @@ num_egenes$tissue=factor(num_egenes$tissue,levels=num_egenes$tissue)
 
 
 # label artery, heart and smooth muscle: 
-num_egenes$Type=ifelse(str_detect(num_egenes$tissue,'Artery|Heart|Colon|Uterus|Esophagus'),'Artery, Heart and Smooth Muscle','Other tissues')
+num_egenes$Type=NULL
+num_egenes$Type='Other'
 num_egenes$Type=ifelse(str_detect(num_egenes$tissue,'HCASMC'),'HCASMC',num_egenes$Type)
-num_egenes$Type=factor(num_egenes$Type,levels=c('HCASMC','Artery, Heart and Smooth Muscle','Other tissues'))
+num_egenes$Type=ifelse(str_detect(num_egenes$tissue,'Artery'),'Artery',num_egenes$Type)
+num_egenes$Type=ifelse(str_detect(num_egenes$tissue,'fibroblasts'),'Fibroblast',num_egenes$Type)
+num_egenes$Type=ifelse(str_detect(num_egenes$tissue,'Heart'),'Heart',num_egenes$Type)
+num_egenes$Type=ifelse(str_detect(num_egenes$tissue,'Colon|Uterus|Esophagus|Stomach|Lung|Vagina|Intestine'),'Smooth Muscle',num_egenes$Type)
+num_egenes$Type=factor(num_egenes$Type,levels=c('HCASMC','Artery','Fibroblast','Heart','Smooth Muscle','Other'))
 
 
 # plot:
+num_egenes$size=ifelse(num_egenes$Type=='Other',1,2)
 hcasmc_egenes=num_egenes[tissue=='HCASMC',num_egenes]
-p=ggplot(num_egenes,aes(x=num_egenes,y=tissue,color=Type,label=num_egenes))+geom_point()+theme_bw()+theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))+xlab('Number of eGenes')+ylab('Tissue')+scale_color_manual(values=c('red','brown','black'))+annotate('text',x=600,y='HCASMC',label=hcasmc_egenes)
+# p=ggplot(num_egenes,aes(x=num_egenes,y=tissue,color=Type,label=num_egenes))+geom_point()+theme_bw()+theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))+xlab('Number of eGenes')+ylab('Tissue')+scale_color_manual(values=c('red','tan4','linen','deeppink','steelblue','black'))+annotate('text',x=600,y='HCASMC',label=hcasmc_egenes)
+p=ggplot(num_egenes,aes(x=num_egenes,y=tissue,color=Type,label=num_egenes,size=size))+geom_point()+theme_bw()+theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))+xlab('Number of eGenes')+ylab('Tissue')+scale_color_manual(values=c(2:6,1))+scale_size(range=c(1,3),guide=F)
 save_plot('../figures/egenes_vs_sample_size/num_egenes_subsampled_52.pdf',p,base_height=6)
 
+
+# get the number of egenes:
+range(num_egenes[Type=='Artery',num_egenes])
+range(num_egenes[Type=='Smooth Muscle',num_egenes])
+range(num_egenes[Type=='Fibroblast',num_egenes])
+range(num_egenes[Type=='Heart',num_egenes])
 
