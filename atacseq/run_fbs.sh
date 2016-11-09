565a3aa6bd64f45a92537917c17b304b1a9cd97e
Bosh Liu <jollier.liu@gmail.com>
2016-11-09 09:35:43 -0800

    new file:   atacseq/run_fbs.sh

---
+++ atacseq/run_fbs.sh
@@ -0,0 +1,61 @@
+# fbs/200212:
+bds ~/atac_dnase_pipelines/atac.bds -species hg19 -nth 12 \
+ -title 200212 -out_dir ~/atac_hcasmc/fbs/200212/out/ \
+ -fastq1_1 ~/atac_hcasmc/fbs/fastq/200212_L2_CGTACTAG_L002_R1.fastq.gz -fastq1_2 ~/atac_hcasmc/fbs/fastq/200212_L2_CGTACTAG_L002_R2.fastq.gz
+
+
+# fbs/2305
+bds ~/atac_dnase_pipelines/atac.bds -species hg19 -nth 12 \
+ -title 2305 -out_dir ~/atac_hcasmc/fbs/2305/out/ \
+ -fastq1_1 ~/atac_hcasmc/fbs/fastq/CA2305-FBS1_S1_concat_R1_001.fastq.gz -fastq1_2 ~/atac_hcasmc/fbs/fastq/CA2305-FBS1_S1_concat_R2_001.fastq.gz \
+ -fastq2_1 ~/atac_hcasmc/fbs/fastq/CA2305-FBS2_S2_concat_R1_001.fastq.gz -fastq2_2 ~/atac_hcasmc/fbs/fastq/CA2305-FBS2_S2_concat_R2_001.fastq.gz \
+ -fastq3_1 ~/atac_hcasmc/fbs/fastq/CA2305-FBS3_S3_concat_R1_001.fastq.gz -fastq3_2 ~/atac_hcasmc/fbs/fastq/CA2305-FBS3_S3_concat_R2_001.fastq.gz
+
+
+# fbs/1346: 
+bds ~/atac_dnase_pipelines/atac.bds -species hg19 -nth 12 \
+ -title 1346 -out_dir ~/atac_hcasmc/fbs/1346/out/ \
+ -fastq1_1 ~/atac_hcasmc/fbs/fastq/CA1346_L2_TCCTGAGC_L002_R1.fastq.gz -fastq1_2 ~/atac_hcasmc/fbs/fastq/CA1346_L2_TCCTGAGC_L002_R2.fastq.gz
+
+
+# fbs/1508: 
+bds ~/atac_dnase_pipelines/atac.bds -species hg19 -nth 12 \
+ -title 1508 -out_dir ~/atac_hcasmc/fbs/1508/out/ \
+ -fastq1_1 ~/atac_hcasmc/fbs/fastq/CA1508_L1_TAAGGCGA_L001_R1.fastq.gz -fastq1_2 ~/atac_hcasmc/fbs/fastq/CA1508_L1_TAAGGCGA_L001_R2.fastq.gz \
+ -fastq2_1 ~/atac_hcasmc/fbs/fastq/CA1508_L2_TAAGGCGA_L002_R1.fastq.gz -fastq2_2 ~/atac_hcasmc/fbs/fastq/CA1508_L2_TAAGGCGA_L002_R2.fastq.gz
+
+
+# fbs/1522: 
+bds ~/atac_dnase_pipelines/atac.bds -species hg19 -nth 12 \
+ -title 1522 -out_dir ~/atac_hcasmc/fbs/1522/out/ \
+ -fastq1_1 ~/atac_hcasmc/fbs/fastq/CA1522_S8_concat_R1_001.fastq.gz -fastq1_2 ~/atac_hcasmc/fbs/fastq/CA1522_S8_concat_R2_001.fastq.gz
+
+
+# fbs/2108:
+bds ~/atac_dnase_pipelines/atac.bds -species hg19 -nth 12 \
+ -title 2108 -out_dir ~/atac_hcasmc/fbs/2108/out/ \
+ -fastq1_1 ~/atac_hcasmc/fbs/fastq/CA2108_S4_concat_R1_001.fastq.gz -fastq1_2 ~/atac_hcasmc/fbs/fastq/CA2108_S4_concat_R2_001.fastq.gz
+
+
+# fbs/2356: 
+bds ~/atac_dnase_pipelines/atac.bds -species hg19 -nth 12 \
+ -title 2356 -out_dir ~/atac_hcasmc/fbs/2356/out/ \
+ -fastq1_1 ~/atac_hcasmc/fbs/fastq/CA2356_S7_concat_R1_001.fastq.gz -fastq1_2 ~/atac_hcasmc/fbs/fastq/CA2356_S7_concat_R2_001.fastq.gz
+
+
+# fbs/2463:
+bds ~/atac_dnase_pipelines/atac.bds -species hg19 -nth 12 \
+ -title 2463 -out_dir ~/atac_hcasmc/fbs/2463/out/ \
+ -fastq1_1 ~/atac_hcasmc/fbs/fastq/CA2463_S6_concat_R1_001.fastq.gz -fastq1_2 ~/atac_hcasmc/fbs/fastq/CA2463_S6_concat_R2_001.fastq.gz
+
+
+# fbs/2510: 
+bds ~/atac_dnase_pipelines/atac.bds -species hg19 -nth 12 \
+ -title 2510 -out_dir ~/atac_hcasmc/fbs/2510/out/ \
+ -fastq1_1 ~/atac_hcasmc/fbs/fastq/CA2510_S5_concat_R1_001.fastq.gz -fastq1_2 ~/atac_hcasmc/fbs/fastq/CA2510_S5_concat_R2_001.fastq.gz
+
+
+# fbs/2989:
+bds ~/atac_dnase_pipelines/atac.bds -species hg19 -nth 12 \
+ -title 2989 -out_dir ~/atac_hcasmc/fbs/2989/out/ \
+ -fastq1_1 ~/atac_hcasmc/fbs/fastq/CA2989_S7_concat_R1_001.fastq.gz -fastq1_2 ~/atac_hcasmc/fbs/fastq/CA2989_S7_concat_R2_001.fastq.gz
