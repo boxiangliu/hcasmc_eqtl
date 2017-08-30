9d4da8ab2802aad88e1df770dd0bb380e36babf9
jollier.liu@gmail.com <jollier.liu@gmail.com>
2017-08-30 09:37:58 -0700

    added gwas_atacseq_overlap/gregor/parse_encode_vocabulary.py to parse ENCODE human cell types

---
+++ gwas_atacseq_overlap/gregor/parse_encode_vocabulary.py
@@ -0,0 +1,49 @@
+import sys
+
+fn='../data/encode/dnase_seq_2007_2012/controlled_vocabulary/cv.ra'
+
+with open(fn,'r') as f:
+	for line in f:
+		if line.startswith('### Mouse Cell Lines'): break
+		if line.startswith('#'): continue
+
+		if line.strip()=='':
+			if 'out' not in globals(): # header line
+				out_line='term\ttag\ttype\torganism\tdescription\ttissue\tvendorName\tvendorId\torderUrl\tkaryotype\tlineage\ttermId\ttermUrl\tcolor\tsex\ttier\tprotocol\tcategory\n'
+				sys.stdout.write(out_line)
+
+			else: # data line
+				out_line=''
+				for field in ['term',
+							  'tag',
+							  'type',
+							  'organism',
+							  'description',
+							  'tissue',
+							  'vendorName',
+							  'vendorId',
+							  'orderUrl',
+							  'karyotype',
+							  'lineage',
+							  'termId',
+							  'termUrl',
+							  'color',
+							  'sex',
+							  'tier',
+							  'protocol',
+							  'category']:
+					try:
+						out_line+=out[field]+'\t'
+					except KeyError:
+						out_line+='NA\t'
+
+				out_line=out_line.strip()+'\n'
+				sys.stdout.write(out_line)
+
+
+			out=dict() # initialize or re-initialize 'out'
+
+		else:
+			field,annotation=line.strip().split(' ',1)
+			out[field]=annotation
+
